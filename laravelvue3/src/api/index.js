f6d5073c13c69e3651bac974366e00ab269c351c
hagisunandar <150351491+hagisunandar@users.noreply.github.com>
2024-01-01 21:16:45 +0700

    Delete laravelvue3 directory

--- laravelvue3/src/api/index.js
+++
@@ -1,9 +0,0 @@
-//import axios
-import axios from 'axios';
-
-const Api = axios.create({
-    //set default endpoint API
-    baseURL: 'http://localhost:5174/'
-})
-
-export default Api
--- laravelvue3/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router/index';
-
-//create App Vue
-const app = createApp(App);
-app.use(router);
-app.mount('#app');
